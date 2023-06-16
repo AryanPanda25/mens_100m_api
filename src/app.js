7e0b95e302960f03147d16b4d83d1633b6387593
AryanPanda <aryanpanda100@gmail.com>
2023-06-16 23:32:15 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8000
+require("../src/db/conn")
+const MensRanking = require("../src/models/mens")
+const mensRouter = require("./routers/men")
+app.use(express.json());
+app.use(mensRouter);
+app.listen(port,()=>{
+    console.log("listening");
+})
---
+++ src/routers/men.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const router = new express.Router();
+const MensRanking = require("../models/mens");
+
+router.post("/mens",async(req,res)=>{
+    try{
+        const user = new MensRanking(req.body);
+        const result = await user.save();
+        res.status(201).send(result);
+    }
+    catch(err)
+    {
+        res.status(400).send(err);
+    }
+})
+router.get("/mens",async (req,res)=>{
+    try{
+        const result=await MensRanking.find().sort({"ranking":1});
+        res.send(result);
+    }
+    catch(err)
+    {
+        res.status(400).send(err);
+    }
+});
+router.get("/mens/:id",async(req,res)=>{
+    try{
+        const _id=req.params.id;
+        const result=await MensRanking.findById(_id);
+        res.send(result);
+    }
+    catch(err)
+    {
+        res.status(400).send(err);
+    }
+})
+router.patch("/mens/:id",async(req,res)=>{
+    try{
+        const _id = req.params.id;
+        const result=await MensRanking.findByIdAndUpdate(_id,req.body,{new:true});
+        res.send(result);
+    }
+    catch(err)
+    {
+        res.status(500).send(err);
+    }
+})
+router.delete("/mens/:id",async(req,res)=>{
+    try{
+        const _id = req.params.id;
+        const result=await MensRanking.findByIdAndDelete(_id);
+        res.send(result);
+    }
+    catch(err)
+    {
+        res.status(500).send(err);
+    }
+})
+
+module.exports=router;

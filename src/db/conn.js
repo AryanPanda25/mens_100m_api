2e2c7856b296b007337ffd25484ab62dcf4515c9
AryanPanda <aryanpanda100@gmail.com>
2023-06-18 23:44:22 +0530

    ll

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/olympics",{
+mongoose.connect("mongodb://127.0.0.1/olympics",{
     useNewUrlParser:true,
     useUnifiedTopology:true}).then(()=>{
     console.log("connection successfully");

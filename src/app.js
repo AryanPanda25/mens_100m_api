3dcd43a79c104a91730df6ea3e6538b75246cec8
AryanPanda <aryanpanda100@gmail.com>
2023-06-19 00:23:30 +0530

    mno

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const port =8000
-require("../src/db/conn")
-const MensRanking = require("../src/models/mens")
-const mensRouter = require("./routers/men")
+const port = process.env.PORT || 8000;
+require("../src/db/conn");
+const MensRanking = require("../src/models/mens");
+const mensRouter = require("./routers/men");
 app.use(express.json());
 app.use(mensRouter);
 app.listen(port,()=>{
--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/olympics").then(()=>{
+mongoose.connect("mongodb://127.0.0.1:27017/olympics",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true}).then(()=>{
     console.log("connection successfully");
 }).catch((e)=>{
     console.log(e);

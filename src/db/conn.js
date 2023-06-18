ac29d0900386f070a75fb80aa036e2a6bdd1f7d5
AryanPanda <aryanpanda100@gmail.com>
2023-06-18 23:00:47 +0530

    changes

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,12 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/olympics",{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+mongoose.connect("mongodb://127.0.0.1:27017/olympics",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    serverSelectionTimeoutMS: 5000,
+    autoIndex: false, 
+    maxPoolSize: 10,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000}).then(()=>{
     console.log("connection successfully");
 }).catch((e)=>{
     console.log(e);
